ed68512f6c62f55a06202b5cff83c71af05e2b13
egggge <619542721@qq.com>
2019-05-29 20:01:35 +0800

    a

--- src/main/java/GetMaxValue.java
+++ src/main/java/GetMaxValue.java
@@ -21,9 +21,31 @@ public class GetMaxValue {
                 }
                 maxValues[i][j]=Math.max(down,left)+values[i*cols+j];
             }
-
         }
         int maxValue = maxValues[rows-1][cols-1];
         return maxValue;
+    }
+
+    public int getMaxGiftBetter(int[] values,int rows,int cols){
+        if (values==null){
+            return 0;
+        }
+        int[] maxValues = new int[cols];
+        for (int i=0;i<rows;++i){
+            for (int j=0;j<cols;++j){
+                int left=0;
+                int down=0;
+                if (j>0){
+                    left=maxValues[j-1];
+                }
+                if (i>0){
+                    down=maxValues[j];
+                }
+                maxValues[j]=Math.max(left,down)+values[i*cols+j];
+            }
+        }
+        int maxValue=maxValues[cols-1];
+        return maxValue;
+
     }
 }
