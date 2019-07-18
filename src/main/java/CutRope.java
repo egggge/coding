d182e9c78baa931e6f4699aa979ab44fb74bb12b
egggge <619542721@qq.com>
2019-07-18 10:45:41 +0800

    添加注释

--- src/main/java/CutRope.java
+++ src/main/java/CutRope.java
@@ -3,6 +3,11 @@
  * @Date: 2019-05-13 14:23
  */
 public class CutRope {
+    /**
+     * 减绳子，每段长度乘积最大
+     * @param len
+     * @return
+     */
     public static int maxCuttingRope(int len){
         if (len<2){
             return 0;
@@ -22,6 +27,7 @@ public class CutRope {
 
         for (int i=4;i<=len;i++){
             max=0;
+            //这里j<=i/2;因为如果j<i后面数字是重复的
             for (int j=1;j<=i/2;j++){
                 int product = maxProduct[j]*maxProduct[i-j];
                 if (max<product){
