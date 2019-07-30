c1b076a141b17f9d8b4bec833da7f7a9f7135c07
egggge <619542721@qq.com>
2019-07-30 19:22:18 +0800

    新增

---
+++ src/main/java/MaxPrice.java
@@ -0,0 +1,28 @@
+/**
+ * @Author: egg
+ * @Date: 2019-07-30 17:12
+ */
+public class MaxPrice {
+    public static int getMaxPrices(int[] arr){
+        int min=arr[0];
+        int maxPrice=arr[1]-arr[0];
+
+        for (int i=1;i<arr.length;i++){
+            if (min>arr[i]){
+                min=arr[i];
+            }
+            int temp=arr[i]-min;
+            if (maxPrice<temp){
+                maxPrice=temp;
+            }
+        }
+        return maxPrice;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr={9,11,8,5,7,12,16,14};
+        System.out.println(getMaxPrices(arr));
+
+    }
+}
