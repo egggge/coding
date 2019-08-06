9fe3303cb53b0bb50d2da555e5d5b03a86bdd0b0
egggge <619542721@qq.com>
2019-08-06 15:48:23 +0800

    购物单

--- src/main/java/HauWei.java
+++ src/main/java/HauWei.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * @author chendan
@@ -33,6 +31,101 @@ public class HauWei {
             System.out.println(builder.toString());
 
 
+        }
+    }
+
+    /**
+     * s输入：978663
+     * 输出：37879
+     * 避免hashmap的排序，使用LinkehHashMap
+     */
+
+    public void reBuildNum(){
+        Scanner sc = new Scanner(System.in);
+        //hashmap也是自动排序
+        HashMap<Integer,Integer> hashMap=new LinkedHashMap<Integer, Integer>();
+        while(sc.hasNext()) {
+            int n = sc.nextInt();
+            while (n!=0){
+                int temp=n%10;
+                n=n/10;
+                if (!hashMap.containsKey(temp)){
+                    hashMap.put(temp,1);
+                }
+            }
+            StringBuffer str=new StringBuffer();
+            for (Integer key:hashMap.keySet()){
+                str.append(key);
+            }
+            System.out.println(str.toString());
+
+        }
+    }
+    /**
+     * 找出字符的asii码
+     */
+    public void getACSII(){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        for(int i = 0; i < s.length(); i++){
+            System.out.println((int)s.charAt(i));
+        }
+        sc.close();
+
+    }
+
+    /**
+     * 购物单问题（为什么不能写else）
+     * @param N 可花费的最大资金
+     * @param m m种物品选择
+     * @param w 每个物品的权重
+     * @param v 每个物品的价格
+     * @param p 判断是否是主件（==0）还是附件
+     * @return
+     */
+
+    public static int getMaxValue(int N,int m,int[] w,int[] v,int[] p){
+        int[][] res=new int [m+1][N+1];
+        //i表示第几个物品
+        for(int i=1;i<=m;i++){
+            //j表示当前拥有资金
+            for(int j=0;j<=N;j++){
+                //p[1]表示第一个物品是否为主件
+                if(p[i]==0){
+                    //v[1]表示第一个物品的价格
+                    if(v[i]<=j){
+                        res[i][j]=Math.max(res[i-1][j],res[i-1][j-v[i]]+v[i]*w[i]);
+                    }
+                }else{
+                    if(v[i]+v[p[i]]<=j){
+                        res[i][j]=Math.max(res[i-1][j],res[i-1][j-v[i]-v[p[i]]]+v[i]*w[i]+v[p[i]]*w[p[i]]);
+                    }
+                }
+            }
+        }
+        return res[m][N];
+
+    }
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNext()){
+            String s=sc.nextLine();
+            int N=Integer.parseInt(s.split(" ")[0]);
+            int m=Integer.parseInt(s.split(" ")[1]);
+            int i=1;
+            int[] w=new int[m+1];
+            int[] v=new int[m+1];
+            int[] p=new int[m+1];
+            while (i<=m){
+                String input=sc.nextLine();
+                v[i]=Integer.parseInt(input.split(" ")[0]);
+                w[i]=Integer.parseInt(input.split(" ")[1]);
+                p[i]=Integer.parseInt(input.split(" ")[2]);
+                i++;
+            }
+            int result=getMaxValue(N,m,w,v,p);
+            System.out.println(result);
         }
     }
 }
