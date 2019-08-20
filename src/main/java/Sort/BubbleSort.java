e79edd67eddd16e468200407d7b9ee382c2304af
egggge <619542721@qq.com>
2019-08-20 10:21:17 +0800

    判断质数

--- src/main/java/Sort/BubbleSort.java
+++ src/main/java/Sort/BubbleSort.java
@@ -22,11 +22,11 @@ public class BubbleSort {
             return;
         }
         int len=data.length;
-        for (int i = 0; i < len; ++i) {
+        for (int i = 1; i < len; ++i) {
             // 提前退出冒泡循环的标志位,即一次比较中没有交换任何元素，这个数组就已经是有序的了
             boolean flag = false;
             //此处你可能会疑问的j<n-i-1，因为冒泡是把每轮循环中较大的数飘到后面，
-            for (int j = 0; j < len - i - 1; ++j) {
+            for (int j = 0; j < len - i ; ++j) {
                 // 数组下标又是从0开始的，i下标后面已经排序的个数就得多减1，总结就是i增多少，j的循环位置减多少
                 if (data[j] > data[j + 1]) {
                     int temp = data[j];
@@ -40,6 +40,11 @@ public class BubbleSort {
         }
 
     }
+
+    /**
+     * 记录某趟交换的最后位置，可以避免遍历后面的数字
+     * @param data
+     */
     public static void bubbleSortII(int[] data){
         if (data.length==0){
             return;
