5933631a870c3cdd573afc2e7dbabecd0a4bc36c
egggge <619542721@qq.com>
2019-08-21 18:26:12 +0800

    判断质数

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,30 +5,11 @@ import java.util.*;
  * @Date: 2019-06-04 20:56
  */
 public class test {
-    public static int lastWordLen(String s) {
-        if (s == null) {
-            return 0;
-        } else if (s.trim().equals("")) {
-            return 0;
-        } else {
-            String[] l = s.split(" ");
-            return l[l.length - 1].toCharArray().length;
-        }
 
-    }
 
-    public static int containWord(String s1, String s2) {
-        char[] s = s1.toCharArray();
-        char[] target = s2.toCharArray();
-        int count = 0;
-        for (int i = 0; i < s.length; i++) {
-            if (s[i] == target[0]) {
-                count++;
-            }
-        }
-        return count;
 
-    }
+
+
 
 
     public static void sortAscend(Integer[] a) {
@@ -41,87 +22,51 @@ public class test {
 
 
 
-    public static String getItoR(String target, List<String> I){
-        StringBuffer result=new StringBuffer();
-        StringBuffer res=new StringBuffer();
-        int temp=0;
-        for (int s=0;s<I.size();s++){
-            if (I.get(s).contains(target)){
-                res.append(s);
-                res.append(" ");
-                res.append(I.get(s));
-                res.append(" ");
-                temp=temp+2;
-            }
-        }
-        if (temp!=0){
-            result.append(target);
-            result.append(" ");
-            result.append(temp);
-            result.append(" ");
-            result.append(res.toString());
-            result.append(" ");
-        }
-
-        return result.toString();
-
-
-    }
-
 
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         while (sc.hasNext()){
-            Set<Integer> R=new TreeSet<Integer>();
-            List<String> I=new ArrayList<String>();
-            String inputI=sc.nextLine();
-            String inputR=sc.nextLine();
-            int i=Integer.parseInt(inputI.split(" ")[0]);
-            int m=1;
-            while (m<=i){
-                I.add(String.valueOf(inputI.split(" ")[m]));
-                m++;
-            }
-            int j=Integer.parseInt(inputR.split(" ")[0]);
-            int n=1;
-            while (n<=j){
-                R.add(Integer.parseInt(inputR.split(" ")[n]));
-                n++;
-            }
-            int count=0;
-            StringBuffer buffer=new StringBuffer();
-            int postionR=0;
-            while(postionR<j){
-                if (!R.(postionR).equals(R.get(postionR+1))){
-                    String target=String.valueOf(R.get(postionR));
-                    if (getItoR(target,I).length()!=0){
-                        int temp=Integer.parseInt(getItoR(target,I).split(" ")[1]);
-                        buffer.append(getItoR(target,I));
-                        count+=temp+2;
-
-                    }
-                    postionR++;
-                }else {
-                    while (postionR<j-1&&R.get(postionR).equals(R.get(postionR+1))){
-                        postionR++;
-                    }
-                    String target=String.valueOf(R.get(postionR));
-                    if (getItoR(target,I).length()!=0){
-                        int temp=Integer.parseInt(getItoR(target,I).split(" ")[1]);
-                        buffer.append(getItoR(target,I));
-                        count+=temp+2;
-
-                    }
-                    postionR=postionR+1;
-
+            String[] arr=sc.nextLine().split(" ");
+            List<String> output=new ArrayList<String>();
+            int n=Integer.parseInt(arr[0]);
+            for (int i=1;i<=n;i++){
+                StringBuffer s=new StringBuffer();
+                int l=arr[i].length();
+                int record=l/8;
+                int remind=l%8;
+                int k=0;
+                while (record>=1){
+                    output.add(arr[i].substring(k,k+8));
+                   // System.out.println(arr[i].substring(k,k+8));
+                    record--;
+                    k=+8;
                 }
+                if (remind!=0){
+                    String temp=arr[i].substring(k,l);
+                    s.append(temp+"00000000");
+                    output.add(s.toString().substring(0,8));
+                    //System.out.println(s.toString().substring(0,8));
+                }
+                }
+            Collections.sort(output);
+            for (String p:output){
+                System.out.println(p);
+                //System.out.printf(p+" ");
             }
-            System.out.printf(count+" "+buffer.toString());
+            }
 
         }
 
+
+
+
+
+
+
     }
-}
+
+
+
 
 
 
