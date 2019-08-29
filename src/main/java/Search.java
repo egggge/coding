09b8de934a12dd9cd47368c8b737a9d2de8bd7dd
egggge <619542721@qq.com>
2019-08-29 15:32:11 +0800

    从两个排序数列中找出中位数

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -4,6 +4,53 @@
  */
 public class Search {
     /**
+     * 在两个排序数组中找出中位数
+     * @param A
+     * @param B
+     * @return
+     */
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        // to ensure m<=n
+        if (m > n) {
+            int[] temp = A; A = B; B = temp;
+            int tmp = m; m = n; n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            //类似于二分查找
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            //小数组提供的是0-（i-1）
+            //大数组提供的是0-（j-1）
+            if (i < iMax && B[j-1] > A[i]){
+                // i is too small
+                iMin = i + 1;
+            }
+            ////m数组提供少一点
+            else if (i > iMin && A[i-1] > B[j]) {
+                // i is too big
+                iMax = i - 1;
+            }
+            else {
+                int maxLeft = 0;
+                if (i == 0) { maxLeft = B[j-1]; }
+                else if (j == 0) { maxLeft = A[i-1]; }
+                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+                int minRight = 0;
+                if (i == m) { minRight = B[j]; }
+                else if (j == n) { minRight = A[i]; }
+                else { minRight = Math.min(B[j], A[i]); }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+    /**
      * 找关键字为key的数据元素
      * @param data
      * @param key
